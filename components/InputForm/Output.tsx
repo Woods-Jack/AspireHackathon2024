4fa131d7a39919b750e8190d284973838250184c
alexandra-pacuraru-ps <113995542+alexandra-pacuraru-ps@users.noreply.github.com>
2024-01-30 14:49:00 +0000

    push on main bc jack told me so

--- components/InputForm/Output.tsx
+++ components/InputForm/Output.tsx
@@ -1,41 +1,45 @@
-import { Flex, Box } from "@chakra-ui/react";
-
+import { Flex, Box, Heading, VStack, Text } from "@chakra-ui/react";
 
 interface OutputProps {
   content: {
-    title: string,
-    numberOfDays: string,
-    location: string,
-    activities: Activity[]
-  }
+    title: string;
+    numberOfDays: string;
+    location: string;
+    activities: Activity[];
+  };
 }
 interface Activity {
-  title: string,
-  length: string,
-  description: string,
+  title: string;
+  length: string;
+  description: string;
 }
 
-const Output = ({content}:OutputProps) => {
-  const { title, numberOfDays, location, activities } = content || {} ;
+const Output = ({ content }: OutputProps) => {
+  const { title, numberOfDays, location, activities } = content || {};
 
-  return(
+  return (
     <>
       <Flex>
-        <h1>{title}</h1>
-        <p>{numberOfDays}</p>
-        <em>{location}</em>
+        <VStack>
+          <Heading as="h1" size="md" noOfLines={1} pt="10">
+            {title}
+          </Heading>
+          <Text>{numberOfDays}</Text>
+          <Text as='i'>{location}</Text>
+        </VStack>
       </Flex>
-      {activities && activities.map((activity) => (
-        <Box key={activity.title}>
-          <Flex>
-            <h2>{activity.title}</h2>
-            <em>{activity.length}</em>
-          </Flex>
-          <p>{activity.description}</p>
-        </Box>
-      ))}
+      {activities &&
+        activities.map((activity) => (
+          <Box key={activity.title}>
+            <Flex>
+              <h2>{activity.title}</h2>
+              <Text as='i'>{activity.length}</Text>
+            </Flex>
+            <p>{activity.description}</p>
+          </Box>
+        ))}
     </>
-  )
-}
+  );
+};
 
 export default Output;
