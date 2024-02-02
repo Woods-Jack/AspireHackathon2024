8133568f7d090a978413afeef591a12c9b9958e0
Jack Woods <jacwoods1@WKMZT4760AEC.global.publicisgroupe.net>
2024-02-02 10:13:13 +0000

    Working output

--- components/InputForm/Output.tsx
+++ components/InputForm/Output.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, VStack, Heading, Text } from "@chakra-ui/react";
 
 
 interface OutputProps {
@@ -19,22 +19,24 @@ const Output = ({content}:OutputProps) => {
   const { title, numberOfDays, location, activities } = content || {} ;
 
   return(
-    <>
-      <Flex>
-        <h1>{title}</h1>
-        <p>{numberOfDays}</p>
-        <em>{location}</em>
+    <Box w='75vw' bg='#f8f8ff'  p={4} mt={4} rounded='xl'>
+      <Flex pb={4} justify='space-between'>
+        <Heading as='h1'>{title}</Heading>
+        <VStack alignItems='end' spacing={0}>
+          {numberOfDays && <p>{`${numberOfDays} Days`}</p>}
+          <em>{location}</em>
+        </VStack>
       </Flex>
       {activities && activities.map((activity) => (
-        <Box key={activity.title}>
-          <Flex>
-            <h2>{activity.title}</h2>
+        <Box p={2} mt={4} mx={2} key={activity.title} bg='#f3f3f3'> 
+          <Flex justify='space-between' alignItems='end' pb={1}>
+            <Heading as='h2' size='lg'>{activity.title}</Heading>
             <em>{activity.length}</em>
           </Flex>
-          <p>{activity.description}</p>
+          <Text w='75%'>{activity.description}</Text>
         </Box>
       ))}
-    </>
+    </Box>
   )
 }
 
--- langchain/prompts/getItineraryTemplate.js
+++ langchain/prompts/getItineraryTemplate.js
@@ -5,6 +5,7 @@ const getItineraryTemplate = `
   You are an intelligent AI travel agent who is an expert at recommending points of interest and itineraries. 
   You should make recommendations based primarily on the context provided, expanding on this context where necessary for a detailed response.
   You should build an itinerary with these recommendations based on the user's preferences, the length of their stay, and their budget (low, medium or high).
+  Make sure to write a detailed description of the activity.
 
   """
   {context}
