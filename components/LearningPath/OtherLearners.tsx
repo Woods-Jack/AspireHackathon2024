8d851fff8292c656710588a180f76c1f45187e00
alexandra-pacuraru-ps <113995542+alexandra-pacuraru-ps@users.noreply.github.com>
2024-02-04 00:15:52 +0000

    fixes

--- components/LearningPath/OtherLearners.tsx
+++ components/LearningPath/OtherLearners.tsx
@@ -6,20 +6,25 @@ interface LearnerProps {
   learners: UserProfileProps[];
 }
 
-export const OtherLearners = ({learners}:LearnerProps) => {
-
-  return(
-    <VStack alignItems='start'>
-    <Heading as='h3' fontSize='32px' fontWeight={350}>Other SPEED Learners:</Heading>
-    <Box  p={4} mt={2} mb={8} rounded='2xl'>
-      <Flex justifyContent='flex-start' gap={4}>
-        {learners.map((profile:any) => (
-          <Box key={profile.llid}>
-            <p>{profile.name}</p>
-          </Box>
-        ))}
-      </Flex>
-    </Box>
-  </VStack>
-  )
-}
+export const OtherLearners = ({ learners }: LearnerProps) => {
+  return (
+    <VStack alignItems="start" flex='2'>
+      <Heading as="h3" fontSize="32px" fontWeight='medium'>
+        Other SPEED Learners:
+      </Heading>
+      <Box p={4} mt={2} mb={8} rounded="2xl">
+        <Wrap justifyContent="flex-start" gap={4}>
+          {learners.map((member, index) => (
+            <UserProfile
+              key={index}
+              firstName={member.firstName}
+              lastName={member.lastName}
+              location={member.location}
+              bg={color.lightGrey}
+            />
+          ))}
+        </Wrap>
+      </Box>
+    </VStack>
+  );
+};
